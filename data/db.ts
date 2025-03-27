4037ab3ede2d0f4e7f335adefb5bba70d33a4ad0
ilon-trask <misha007zty@gmail.com>
2025-03-27 16:33:30 +0100

    refactor: useDatabase

---
+++ data/db.ts
@@ -0,0 +1,22 @@
+
+const labels = ["chat", "message"];
+
+export async function createDB(): Promise<IDBDatabase> {
+  const db: IDBDatabase | DOMException = await new Promise(
+    (resolve, reject) => {
+      const request = indexedDB.open("DB", 1);
+      request.onupgradeneeded = (event) => {
+        const db = event.target?.result;
+        labels.forEach((label) => {
+          if (!db.objectStoreNames.contains(label)) {
+            db.createObjectStore(label, { keyPath: "id" });
+          }
+        });
+      };
+      request.onsuccess = () => resolve(request.result);
+      request.onerror = () => reject(request.error);
+    }
+  );
+  if (db instanceof DOMException) throw db;
+  return db;
+}
--- hooks/useDatabase.ts
+++ hooks/useDatabase.ts
@@ -1,33 +1,14 @@
+import { createDB } from "@/data/db";
 import { useEffect, useState } from "react";
-
-const labels = ["chat", "message"];
 
 export function useDatabase() {
   const [isLoading, setIsLoading] = useState(true);
   const [db, setDb] = useState<IDBDatabase | null>(null);
   useEffect(() => {
-    (async () => {
-      const db: IDBDatabase | DOMException = await new Promise(
-        (resolve, reject) => {
-          const request = indexedDB.open("DB", 1);
-          request.onupgradeneeded = (event) => {
-            const db = event.target?.result;
-            labels.forEach((label) => {
-              if (!db.objectStoreNames.contains(label)) {
-                db.createObjectStore(label, { keyPath: "id" });
-              }
-            });
-          };
-          request.onsuccess = () => resolve(request.result);
-          request.onerror = () => reject(request.error);
-        }
-      );
-      if (db instanceof DOMException) {
-        throw db;
-      }
+    createDB().then((db) => {
       setIsLoading(false);
       setDb(db);
-    })();
+    });
   }, []);
   return { isLoading, db };
 }
