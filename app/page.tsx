933a64e68ff17ba54e10ad3e8945144e1f39953c
ilon-trask <misha007zty@gmail.com>
2025-03-27 15:04:29 +0100

    chore: added dbContext

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,32 @@
-'use client'
-import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+"use client";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import Sidebar from "@/components/Sidebar";
 import ChatHeader from "@/components/ChatHeader";
 import MessagesList from "@/components/MessagesList";
 import MessageInput from "@/components/MessageInput";
-import { useDatabase } from "@/hooks/useDatabase";
-import { useEffect } from "react";
+import { DbProvider } from "@/contexts/dbContext";
 
 export default function Home() {
-  const { isLoading, db } = useDatabase();
-  useEffect(()=>{console.log(isLoading,db)},[isLoading])
-  if(isLoading) return <div>Loading...</div>
-  if(!db) return <div>Error</div>
-  // db.transaction('chat)
   return (
-    <ResizablePanelGroup direction="horizontal" className="h-screen">
-      {/* Sidebar */}
-      <ResizablePanel defaultSize={30} minSize={15} collapsible>
-        <Sidebar />
-      </ResizablePanel>
-      {/* <div> some test</div> */}
-      <ResizableHandle />
-      {/* Main Content */}
-      <ResizablePanel className="flex flex-col">
-        <ChatHeader />
-        <MessagesList />
-        <MessageInput />
-      </ResizablePanel>
-    </ResizablePanelGroup>
+    <DbProvider>
+      <ResizablePanelGroup direction="horizontal" className="h-screen">
+        {/* Sidebar */}
+        <ResizablePanel defaultSize={30} minSize={15} collapsible>
+          <Sidebar />
+        </ResizablePanel>
+        {/* <div> some test</div> */}
+        <ResizableHandle />
+        {/* Main Content */}
+        <ResizablePanel className="flex flex-col">
+          <ChatHeader />
+          <MessagesList />
+          <MessageInput />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </DbProvider>
   );
 }
---
+++ contexts/dbContext.tsx
@@ -0,0 +1,19 @@
+import { useDatabase } from "@/hooks/useDatabase";
+import { createContext, useContext } from "react";
+
+export const dbContext = createContext<IDBDatabase | null>(null);
+
+export function useDBContext() {
+  const db = useContext(dbContext);
+  if (!db) {
+    throw new Error("useDatabase must be used within a dbContext");
+  }
+  return db;
+}
+
+export function DbProvider({ children }: { children: React.ReactNode }) {
+  const { isLoading, db } = useDatabase();
+  if (isLoading) return <div>Loading...</div>;
+  if (!db) throw new Error("Database not found");
+  return <dbContext.Provider value={db}>{children}</dbContext.Provider>;
+}
