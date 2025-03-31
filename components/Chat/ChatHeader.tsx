13eb169b5ec37442ae12b19e8f331fc2ba786ca3
ilon-trask <misha007zty@gmail.com>
2025-03-31 13:38:01 +0200

    refactor: show chat name in ChatHeader

--- components/Chat/ChatHeader.tsx
+++ components/Chat/ChatHeader.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Large} from "../Typography";
+import { Large } from "../Typography";
+import { useChatStore } from "@/store/chatStore";
 
 export default function ChatHeader() {
+  const chatStore = useChatStore();
+
+  const chat = chatStore.getChatById(chatStore.chosenChatId);
   return (
     <div className="h-[73px]  border-b border-gray-200 dark:border-gray-800 px-4 flex items-center">
-      <Large>Selected Chat Name</Large>
+      <Large>{chat?.name || "Select a chat"}</Large>
     </div>
   );
-} 
+}
