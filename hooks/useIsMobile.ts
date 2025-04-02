b9b8ff13d6c260582ee861641969db965fbfb3a3
ilon-trask <misha007zty@gmail.com>
2025-04-02 12:25:55 +0200

    fix: resolving error in useIsMobile

--- hooks/useIsMobile.ts
+++ hooks/useIsMobile.ts
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 
 export default function useIsMobile() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMobile, setIsMobile] = useState(false);
 
   const updateSize = () => {
     setIsMobile(window.innerWidth < 768);
   };
 
   useEffect(() => {
-    window.addEventListener("resize", updateSize);
-    return (): void => window.removeEventListener("resize", updateSize);
+    if (typeof window !== "undefined") {
+      updateSize();
+
+      window.addEventListener("resize", updateSize);
+      return (): void => window.removeEventListener("resize", updateSize);
+    }
   }, []);
 
   return isMobile;
