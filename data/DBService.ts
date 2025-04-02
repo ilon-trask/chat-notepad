e726ce9670f90eae434d81401b7fd276c3827264
ilon-trask <misha007zty@gmail.com>
2025-04-02 18:17:24 +0200

    fix: DBService db init

--- data/DBService.ts
+++ data/DBService.ts
@@ -3,16 +3,11 @@ import { createDB } from "./db";
 
 export class DBService<T> {
   private _label: string;
-  private _db: IDBDatabase;
+  private _db: Promise<IDBDatabase>;
 
   constructor(label: string) {
     this._label = label;
-    this._db = {} as IDBDatabase; // Initialize with empty object to satisfy TypeScript
-
-    // Initialize the database asynchronously
-    (async () => {
-      this._db = await createDB();
-    })();
+    this._db = createDB();
   }
 
   protected _changeDBType(chatDB: IDBObjectStore) {
@@ -27,39 +22,45 @@ export class DBService<T> {
     return chatDB as ChatDB;
   }
 
-  protected _getReadDbObject() {
-    const transaction = this._db.transaction(this._label, "readonly");
+  protected async _getReadDbObject() {
+    const transaction = (await this._db).transaction(this._label, "readonly");
     const DB = transaction.objectStore(this._label);
     return this._changeDBType(DB);
   }
 
-  protected _getWriteDbObject() {
-    const transaction = this._db.transaction(this._label, "readwrite");
+  protected async _getWriteDbObject() {
+    const transaction = (await this._db).transaction(this._label, "readwrite");
     const DB = transaction.objectStore(this._label);
     return this._changeDBType(DB);
   }
 
-  protected async getAll() {
-    const DB = this._getReadDbObject();
+  async getAll() {
+    const DB = await this._getReadDbObject();
     const req = DB.getAll();
     const res = await DBResPromise(req);
     return res;
   }
 
-  protected async create(data: T) {
-    const DB = this._getWriteDbObject();
+  async create(data: T) {
+    const DB = await this._getWriteDbObject();
     const req = DB.add(data);
     await DBResPromise(req);
   }
 
-  protected async delete(id: string) {
-    const DB = this._getWriteDbObject();
+  async delete(id: string) {
+    const DB = await this._getWriteDbObject();
     const req = DB.delete(id);
     await DBResPromise(req);
   }
 
-  protected async update(data: T) {
-    const DB = this._getWriteDbObject();
+  async deleteAll() {
+    const DB = await this._getWriteDbObject();
+    const req = DB.clear();
+    await DBResPromise(req);
+  }
+
+  async update(data: T) {
+    const DB = await this._getWriteDbObject();
     const req = DB.put(data);
     await DBResPromise(req);
   }
